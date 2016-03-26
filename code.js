d54140d028cb7baf37d62e153508240e34dd4998
18praveenb <Praveen@Shire-2.local>
2016-03-26 11:54:50 -0700

    OKAY this is meaningufl

--- code.js
+++ code.js
@@ -1,21 +1,45 @@
 var recorder;
+var context;
+
+function dgid(id) {
+    return document.getElementById(id);
+}
+
+function ga(a, b) {
+    return a.getAttribute(b);
+}
+
+function dga(a, b) {
+    return ga(dgid(a), b);
+}
+
+function sa(a, b, c) {
+    a.setAttribute(b, c);
+}
+
+function dsa(a, b, c) {
+    sa(dgid(a), b, c);
+}
 
 function record() {
-    var context = new AudioContext();
+    context = new AudioContext();
     var promise = navigator.mediaDevices.getUserMedia({audio: true, video: false});
     promise.then(function (stream) {
                  recorder = new Recorder(context.createMediaStreamSource(stream));
                  recorder.record();
                  })
     
-    promise.catch(function (error) { console.log("smth wnt wrng wit get mic") }); //Mozeela
+    promise.catch(function (error) { console.log("record() failed") }); //Mozeela
 }
 
 function stop() {
     recorder.stop();
-    recorder.exportWAV(this.config.callback(), WAV)
-    recorder.encodeAfterRecorder();
-    recorder.forceDownload(blob, output)
+    recorder.exportWAV(deal);
+}
+
+function deal(blob) {
+    var url = URL.createObjectURL(blob);
+    dgid("test").textContent = url;
 }
 
 function upload() {}
