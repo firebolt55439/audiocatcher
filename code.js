fe9e2e27bd1294d33d60c904e1311aa1b2359691
18praveenb <Praveen@Shire-2.local>
2016-03-26 10:46:11 -0700

    webaudio doesnt work

--- code.js
+++ code.js
@@ -1,17 +1,18 @@
 function record() {
     console.log("hellow")
-var recorder = new Recorder(source ,this.config);  
-recorder.record();
-recorder.stop();
-recorder.exportWAV(this.config.callback(),WAV)
-  recorder.encodeAfterRecorder();
-  Recorder.forceDownload(blob, output)
-  
-});
+	var context = window.audioContext() || window.webkitAudioContext();
+    var recorder = new Recorder(source , this.config);
+    recorder.record();
+    recorder.stop();
+    recorder.exportWAV(this.config.callback(), WAV)
+    recorder.encodeAfterRecorder();
+    Recorder.forceDownload(blob, output)
 }
 
 function upload() {}
 
 function soundcloud() {}
+
+
 
 
