fac5d670a9145c231a2688d6ad077965d6d3e7da
18praveenb <Praveen@Shire-2.local>
2016-03-26 10:15:29 -0700

    asdf

--- code.js
+++ code.js
@@ -1,19 +1,12 @@
 function record() {
-    console.log("hellow")
-    <script src="javascripts/WebAudioRecorder.js"></script>
-    audioRecorder = new WebAudioRecorder(sourceNode, {
-  workerDir: "javascripts/"    
-  recorder = new WebAudioRecorder(sourceNode, configs)
-  recorder.timeLimit(600);
-  recorder.startRecording();
-  recorder.finishRecording();
-  recorder.encodeAfterRecorder();
-  
-});
+    console.log("hellow");
 }
 
 function upload() {}
 
 function soundcloud() {}
+
+
+
 
 
