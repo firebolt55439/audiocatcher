4a4eb9142dc84e1ef349eaa74b9fdbe6287aec0d
18vijayb <18vijayb@students.harker.org>
2016-03-26 10:20:55 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,19 +1,17 @@
 function record() {
     console.log("hellow")
-var recorder = new Recorder(source [, config])  
+var recorder = new Recorder(source ,this.config);  
 recorder.record();
 recorder.stop();
 recorder.exportWAV(this.config.callback(),WAV)
   recorder.encodeAfterRecorder();
   Recorder.forceDownload(blob, output)
+  
 });
 }
 
 function upload() {}
 
 function soundcloud() {}
-
-
-
 
 
