87e304f9ad6667f0631087137352e208ad97ab17
18vijayb <18vijayb@students.harker.org>
2016-03-26 09:59:51 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,15 @@
 function record() {
     console.log("hellow")
+    <script src="javascripts/WebAudioRecorder.js"></script>
+    audioRecorder = new WebAudioRecorder(sourceNode, {
+  workerDir: "javascripts/"    
+  recorder = new WebAudioRecorder(sourceNode, configs)
+  recorder.timeLimit(600);
+  recorder.startRecording();
+  recorder.finishRecording();
+  recorder.encodeAfterRecorder();
+  
+});
 }
 
 function upload() {}
